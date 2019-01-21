fe322d3e5aa8c5e5e5f4836593f65f7708a2e1e4
ramosjm <jjramos_2004@hotmail.com>
2019-01-21 15:08:54 +0000

    updated code to reflect players as an array.

--- specs/classes_lab_b_specs.rb
+++ specs/classes_lab_b_specs.rb
@@ -5,19 +5,19 @@ require_relative('../classes_lab_b.rb')
 class MyTeamTest < MiniTest::Test
 
   def setup()
-    @team_info = MyTeam.new("Yorkies","9","Mike Douglas")
+    @team_info = MyTeam.new("Yorkies",["dave","nathan","bob"],"Mike Douglas")
   end
 
   def test_get_team_name()
     assert_equal("Yorkies",@team_info.team_name)
   end
-  
+
   def test_get_players()
-    assert_equal("9",@team_info.players)
+    assert_equal(["dave", "nathan", "bob"],@team_info.players)
   end
 
   def test_get_coach()
-    assert_equal("Mike Douglas",@team_info.coach)
+    assert_equal("Mike Douglas", @team_info.coach)
   end
 
   def test_set_coach_name()
