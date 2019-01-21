9dd3a47ee5bfa98ea0dc6f7446f99f48e39051e6
ramosjm <jjramos_2004@hotmail.com>
2019-01-21 14:47:51 +0000

    Part B get team name test passed.

---
+++ classes_lab_b.rb
@@ -0,0 +1,14 @@
+class MyTeam
+
+  def initialize(team_name,players,coach)
+    @team_name = team_name
+    @players = players
+    @coach = coach
+  end
+
+  def get_team_name()
+    return @team_name
+  end
+
+  
+end
---
+++ specs/classes_lab_b_specs.rb
@@ -0,0 +1,15 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../classes_lab_b.rb')
+
+class MyTeamTest < MiniTest::Test
+
+  def setup()
+    @team_info = MyTeam.new("Yorkies","9","Mike Douglas")
+  end
+
+  def test_get_team_name()
+    assert_equal("Yorkies",@team_info.get_team_name)
+  end
+
+end
